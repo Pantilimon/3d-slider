d8b2afbcd6a0dd3d4e6243ae77309cf413fcf2ad
”Anton <limonovtosha3228@gmail.com>
2024-06-18 14:13:20 +0300

    public folder

--- script.js
+++ script.js
@@ -5,44 +5,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const headDescription = document.getElementById('head-description');
   const characterDescription = document.getElementById('character-description');
   const characterImage = document.getElementById('character-image');
-  const characterEl = document.getElementById('elements');
+  const but = document.querySelector('.diamond-btn')
+
   
   let selectedCard = null; // Переменная для хранения ссылки на текущую выбранную карточк
 
   // Определяем объекты, содержащие данные для каждого персонажа
+  // Оказывается, можно менять цвет у вектора, а не скачивать новое изображение... и тогда код на строчки 7 корчое и правильнее будет, но менять мне лень)
   const characterData = {
     character1: {
       description: 'Утратив Искру, мы лишились возможности вернуть нашу планету к жизни. Но судьба в награду подарила нам новый мир, который мы назовём своим домом. Теперь мы живём среди людей, скрывая свое истинное лицо, и тайно за ними наблюдаем… Ждём, защищаем. Я увидел, на какую отвагу они способны, и хотя нас разделяют миры, с ними, как и с нами всё не так.',
       head: 'Огонь - программист', 
-      image: 'img/fireman.png',
-      imageEl: 'img/ElementsFire.png',
-      useBut: 'img/But/ButFire.png',
-      But: 'img/But/fire.png',
+      image: 'public/img/fireman.png',
+      useBut: 'public/img/But/ButFire.svg',
+      But: 'public/img/But/fire.svg',
+      vec: 'btn1',
     },
     character2: {
       description: ' Утратив Искру, мы лишились возможности вернуть нашу планету к жизни. Но судьба в награду подарила нам новый мир, который мы назовём своим домом. Теперь мы живём среди людей, скрывая свое истинное лицо, и тайно за ними наблюдаем… Ждём, защищаем. Я увидел, на какую отвагу они способны, и хотя нас разделяют миры, с ними, как и с нами всё не так.',
       head: 'Вода - жидкий',
-      image: 'img/waterman.png',
-      imageEl: 'img/ElementsWater.png',
-      useBut: 'img/But/ButWater.png',
-      But: 'img/But/water.png',
+      image: 'public/img/waterman.png',
+      useBut: 'public/img/But/ButWater.svg',
+      But: 'public/img/But/water.svg',
+      vec: 'btn4',
       
     },
     character3: {
       description: ' Утратив Искру, мы лишились возможности вернуть нашу планету к жизни. Но судьба в награду подарила нам новый мир, который мы назовём своим домом. Теперь мы живём среди людей, скрывая свое истинное лицо, и тайно за ними наблюдаем… Ждём, защищаем. Я увидел, на какую отвагу они способны, и хотя нас разделяют миры, с ними, как и с нами всё не так.', 
       head: 'Зе(м)ля - хто я?',
-      image: 'img/earthman.png',
-      imageEl: 'img/ElementsEarth.png',
-      useBut: 'img/But/ButEarth.png',
-      But: 'img/But/earth.png',
+      image: 'public/img/earthman.png',
+      useBut: 'public/img/But/ButEarth.svg',
+      But: 'public/img/But/earth.svg',
+      vec: 'btn2',
     },
     character4: {
       description: 'Утратив Искру, мы лишились возможности вернуть нашу планету к жизни. Но судьба в награду подарила нам новый мир, который мы назовём своим домом. Теперь мы живём среди людей, скрывая свое истинное лицо, и тайно за ними наблюдаем… Ждём, защищаем. Я увидел, на какую отвагу они способны, и хотя нас разделяют миры, с ними, как и с нами всё не так.',
       head: 'Воздух - воздухан',
-      image: 'img/wingman.png',
-      imageEl: 'img/ElementsWind.png',
-      useBut: 'img/But/ButWind.png',
-      But: 'img/But/wind.png',
+      image: 'public/img/wingman.png',
+      useBut: 'public/img/But/ButWind.svg',
+      But: 'public/img/But/wind.svg',
+      vec: 'btn3',
     },
   };
 
@@ -54,13 +56,17 @@ document.addEventListener('DOMContentLoaded', () => {
       if (selectedCard === card) {
         // Если уже выбран, снимаем выбор
         selectedCard.querySelector('.button-img').src = characterData[selectedCharacter].But;
+        selectedCard.querySelector('.button-img').classList.toggle('shadow')
         selectedCard.classList.remove('selected');
         headDescription.textContent = 'Общая информация';
         characterDescription.textContent = 'Утратив Искру, мы лишились возможности вернуть нашу планету к жизни. Но судьба в награду подарила нам новый мир, который мы назовём своим домом. Теперь мы живём среди людей, скрывая свое истинное лицо, и тайно за ними наблюдаем… Ждём, защищаем. Я увидел, на какую отвагу они способны, и хотя нас разделяют миры, с ними, как и с нами всё не так.';
-        characterEl.src = 'img/ElementsAll.png';
         characterInfo.className = 'character-info';
         characterDescription.className = 'character-description';
         selectedCard = null;
+
+        document.querySelector(`.${characterData[selectedCharacter].vec}`).classList.toggle('active')
+
+
         // Добавляем класс для анимации исчезания изображения
         characterImage.classList.add('image-animate-out'); 
         characterImage.addEventListener('animationend', () => {
@@ -70,8 +76,10 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         }, { once: true });
       } else {
-        // Снимаем выбор с предыдущей карточки, если она была выбрана
+        // Снимаем выбор (тень, цвет, меняем розу) с предыдущей карточки, если она была выбрана
         if (selectedCard) {
+          document.querySelector(`.${characterData[selectedCard.dataset.character].vec}`).classList.toggle('active')
+          selectedCard.querySelector('.button-img').classList.toggle('shadow')
           selectedCard.querySelector('.button-img').src = characterData[selectedCard.dataset.character].But;
           selectedCard.classList.remove('selected');
         }
@@ -80,25 +88,24 @@ document.addEventListener('DOMContentLoaded', () => {
         card.classList.add('selected');
         selectedCard = card;
         // Обновляем информацию о персонаже
+
+
+        document.querySelector(`.${characterData[selectedCharacter].vec}`).classList.toggle('active')
+        // but.classList.toggle('active')
+
         headDescription.textContent = characterData[selectedCharacter].head;
         characterDescription.textContent = characterData[selectedCharacter].description;
         // Меняем картинки персонажей и элементов
-        characterEl.src = characterData[selectedCharacter].imageEl;
         characterImage.src = characterData[selectedCharacter].image;
-        // Меняем кнопку на нажатую
+        // Меняем кнопку на нажатую !!!!!!!!!!!!!!!!!!!!!!!!!!
         card.querySelector('.button-img').src = characterData[selectedCharacter].useBut;
+        // Анимация выезжания тени у кнопки
+        card.querySelector('.button-img').classList.toggle('shadow')
         // Добавляем анимацию к изображению
         characterImage.classList.remove('image-animate'); // Убираем предыдущую анимацию
         void characterImage.offsetWidth; // Перезапускаем анимацию
         characterImage.classList.add('image-animate');
-        // Добавляем анимацию к element
-        сard.querySelector('.button-img').classList.add('element-animate');
-        сard.querySelector('.button-img').addEventListener('animationElend', () => {
-          if (characterImage.classList.contains('element-animate')) {
-            characterImage.src = characterData[selectedCharacter].useBut;
-            characterImage.classList.remove('element-animate');
-          }
-        }, { once: true });
+        // Добавляем анимацию к элементу
         // Добавляем класс, чтобы применить стили css
         characterInfo.className = selectedCharacter;
       }
